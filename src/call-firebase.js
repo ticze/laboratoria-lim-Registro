fe2324d72ca5b868796c92dfad234dd906b6560c
ticze <ticze2598@gmail.com>
2018-08-05 10:02:18 -0500

    llamando firebase

---
+++ src/call-firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD0R10rhCESqMvr6WvnteQeL2XF-6aDNHc",
+  authDomain: "registro-1f31b.firebaseapp.com",
+  databaseURL: "https://registro-1f31b.firebaseio.com",
+  projectId: "registro-1f31b",
+  storageBucket: "registro-1f31b.appspot.com",
+  messagingSenderId: "622021179466"
+};
+firebase.initializeApp(config);
