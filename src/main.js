fd0d048322135eb084b55a45f9e1bfea705c3af2
ticze <ticze2598@gmail.com>
2018-08-06 09:16:31 -0500

    primer cambio

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 const name = document.getElementById('nombre').value;
 const dni = document.getElementById('dni').value;
 const email = document.getElementById('email').value;
-const btnRegistrar = document.getElementById('registrar')
+const selecRegistrar = document.getElementById('persona')
 
 // Funciones Firebase
 // Get a reference to the database service
 var database = firebase.database();
-btnRegistrar.addEventListener('click' ,()=>{
-  
-  function writeUserData(userId, name, email, dni) {
-    firebase.database().ref('users/' + userId).set({
-      username: name,
+// function writeUserData(userId, name, email, dni) {
+//   firebase.database().ref('users/' + userId).set({
+//     username: name,
+//     dni: dni,
+//     email: email
+//   });
+// }
+btnRegistrar=()=>{
+  event.preventDefault();
+  console.log(name);
+  console.log(dni);
+  console.log(email);
+  console.log(selecRegistrar.value);
+      firebase.database().ref('users/').child('visitante').push({
+      name: name,
       dni: dni,
-      email: email
-    });
-  }
-})
+      email: email,
+      personaAVisitar: persona
+    })
+}
