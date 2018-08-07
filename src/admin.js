4ef4022a7e8de791618361cdb20fef5a963384f6
ticze <ticze2598@gmail.com>
2018-08-07 08:55:25 -0500

    confirmado con firebase e interfaz

--- src/admin.js
+++ src/admin.js
@@ -1,6 +1,6 @@
 const btnInicioHeader = document.getElementById("btn-inicio-header");
 btnInicioHeader.addEventListener('click', e =>{
-    window.location= "interfaz-uno.html"
+    window.location= "index.html"
 })
 
 const btnIngresar = document.getElementById("btn-ingresar");
