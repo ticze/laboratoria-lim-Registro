15ee4a6136f75f3718284c03495899b70952227f
ticze <ticze2598@gmail.com>
2018-08-07 13:20:05 -0500

    2

--- src/admin.js
+++ src/admin.js
@@ -2,5 +2,5 @@ const btnInicioHeader = document.getElementById("btn-inicio-header");
 btnInicioHeader.addEventListener('click', e =>{
     window.location= "index.html"
 })
-
 const btnIngresar = document.getElementById("btn-ingresar");
+
