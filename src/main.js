482d9a2e2c9fdfb610ad4425ef05a456c946d5d0
ticze <ticze2598@gmail.com>
2018-08-07 08:54:17 -0500

    interfas con firebase

--- src/main.js
+++ src/main.js
@@ -1,22 +1,19 @@
-
-const btnRegistro = document.getElementById('registrar')
+const btnRegistro = document.getElementById('btn-ingresar')
 btnRegistro.addEventListener('click', () => {
   event.preventDefault();
-  const name = document.getElementById('nombre').value;
+  const name = document.getElementById('name').value;
   const dni = document.getElementById('dni').value;
-  const email = document.getElementById('email').value;
-  const selecRegistrar = document.getElementById('persona').value;
+  const email = document.getElementById('phone').value;
   console.log(name);
   console.log(dni);
   console.log(email);
-  console.log(selecRegistrar);
+
   // var database = firebase.database();
   const dataVisitante = firebase.database().ref().child('visitante');
   dataVisitante.push({
     name: name,
     dni: dni,
     email: email,
-    personaAVisitar: selecRegistrar,
   })
 
 })
